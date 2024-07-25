68ea37c3f7c5f093f9567265fbee0427329e004e
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-25 23:40:50 +0300

    Update ColorizeTable.js

--- ColorizeTable.js
+++ ColorizeTable.js
@@ -1,7 +1,7 @@
 function colorize() {
-    let rows = document.querySelectorAll(`tr`)
+    let rows = document.querySelectorAll(`tr`);
     for(let i=1; i<rows.length; i+=2){
         rows[i].style.backgroundColor=`orange`
-    }
+    };
     console.log(rows);
 }
